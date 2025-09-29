f9aca3a64699f33bd1be1cdf91b9fcfe7b3907d8
Andre Philippe De Guzman <140810930+Dre12378@users.noreply.github.com>
2025-09-29 20:45:38 +0800

    Test

--- src/constants/index.js
+++ src/constants/index.js
@@ -100,7 +100,7 @@ export const myProjects = [
         ],
     },
     {
-        title: 'Coming Soon - still not stable',
+        title: 'Megaworld BGC - under production',
         desc: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         subdesc:
             'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
