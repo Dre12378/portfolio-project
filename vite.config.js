b73b06673d677db9dcfe99514c3da18850b6feac
Andre Philippe De Guzman <140810930+Dre12378@users.noreply.github.com>
2025-10-01 00:35:36 +0800

    For Prod and Local

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio-project/"
-})
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    // This is the configuration for the local dev server (`npm run dev`)
+    return {
+      plugins: [react()],
+      base: '/portfolio-project/' // The base path that works for your localhost
+    };
+  } else {
+    // This is the configuration for the build command (`npm run build`)
+    return {
+      plugins: [react()],
+      base: '/' // The base path that works for Vercel
+    };
+  }
+});
